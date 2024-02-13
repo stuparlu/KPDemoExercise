0cca42e76d65548f65dbce40ebc1bad96db156c0
Luka Stupar <stupar22luka@gmail.com>
2024-02-13 10:54:44 +0100

    Initial Commit

---
+++ KPDemoExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KPDemoExercise
+//
+//  Created by Luka Stupar on 13.2.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
